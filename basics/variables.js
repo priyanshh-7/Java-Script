6615ec0ad3ed73d2c75bcaaf5a45fe09866ca783
priyanshh-7 <priyanshmishra338@gmail.com>
2024-07-29 04:21:31 +0000

    ok

---
+++ basics/variables.js
@@ -0,0 +1,10 @@
+const accountId="5678"
+let accountEmail="priyansh@google.com"
+var accountPassword="12345"
+accountCity="jaipur"
+let accountState;
+
+accountEmail="ax@gmail.com"
+accountPassword="890"
+accountCity="gorakhpur"
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
